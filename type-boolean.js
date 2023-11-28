2e82e67165ea4324eada92e9bcb97724f0bd1cd7
Diego <diegocaxiado@gmail.com]>
2023-11-28 13:46:41 -0300

    exemplos da aula1

---
+++ type-boolean.js
@@ -0,0 +1,15 @@
+// bboolean
+// true -> verdadeiro
+// faalse->
+
+const pprimeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivo = true;
+
+//console.log(pprimeiroNumero === segundoNumero)
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+
+console.log(texto1 === texto2)
---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Ola mundo!";
+const texto2 = "Ola mundo!";
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const ccitacao = "Meu nome é";
+const meuNome = "Diego";
+
+// concatenação (+)
+
+console.log(ccitacao + meuNome)
+
+// templete string OU template Literal
